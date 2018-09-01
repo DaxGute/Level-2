c8e9220f048298784a2c81f161353984218d20a9
Kendra Kleber <league@Mac-mini-2.local>
2018-09-01 10:50:06 -0700

    window created

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,6 +1,17 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame;
+	final int WINDOWX = 720;
+	final int WINDOWY = 480;
 	public static void main(String[] args) {
-		
+		LeagueInvaders invaders = new LeagueInvaders();
+		invaders.setup();
+	}
+	private  void setup() {
+		frame = new JFrame("League Invaders");
+		frame.setSize(WINDOWX, WINDOWY);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 }

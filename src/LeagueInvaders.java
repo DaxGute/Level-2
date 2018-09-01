52f3bf0cb71c42cdfc3010bbc8c948fbcfa8cb11
Kendra Kleber <league@Mac-mini-2.local>
2018-09-01 11:08:33 -0700

    fix game panel intro

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -2,15 +2,15 @@ import javax.swing.JFrame;
 
 public class LeagueInvaders {
 	JFrame frame;
-	final int WINDOWX = 720;
-	final int WINDOWY = 480;
+	final int WINDOW_X = 720;
+	final int WINDOW_Y = 480;
 	public static void main(String[] args) {
 		LeagueInvaders invaders = new LeagueInvaders();
 		invaders.setup();
 	}
 	private  void setup() {
 		frame = new JFrame("League Invaders");
-		frame.setSize(WINDOWX, WINDOWY);
+		frame.setSize(WINDOW_X, WINDOW_Y);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}

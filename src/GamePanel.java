599063310859c0349450ec91e70395860741aaad
DaxGute <league@Mac-mini-3.local>
2018-09-08 11:44:50 -0700

    keyListener

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,20 +1,44 @@
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Timer time;
 	public GamePanel () {
 		time = new Timer(1000/60, this);
 	}
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println("hi");
+	
+		repaint();
 	}
 	public void startGame() {
 		time.start();
+	}
+	@Override
+
+	public void paintComponent(Graphics g){
+		g.fillRect(10, 10, 100, 100);
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("type");
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("press");
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("release");
+		
 	}
 	
 	
